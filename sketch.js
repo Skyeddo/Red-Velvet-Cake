bbb48ff54ef804ff85c3684ade7208469601e8c7
Skyeddo <67031087+Skyeddo@users.noreply.github.com>
2020-07-04 10:01:05 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var cake1, cake2, candle;
+
+function setup() {
+  createCanvas(800,400);
+
+  cake1 = createSprite (400, 250, 150, 50);
+  cake1.shapeColor="red";
+
+  cake2 = createSprite (400, 200, 100, 50);
+  cake2.shapeColor="blue";
+
+  candle = createSprite (400, 160, 10, 30);
+  candle.shapeColor="yellow";
+
+ 
+
+}
+
+function draw() {
+  //create background
+  background(255,255,255);
+
+
+  drawSprites();
+}
+
